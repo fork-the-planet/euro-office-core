8817a32900e74b3a83fa6ccdc2875ffb968e4a69
Hendrik Leidinger <hendrik.leidinger@nextcloud.com>
2026-01-31 11:11:05 -0800

    wasm build process for hash
    Signed-off-by: Hendrik Leidinger <hendrik.leidinger@nextcloud.com>

---
+++ DesktopEditor/xmlsec/src/wasm/3rdParty/nc-build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+OPENSSL_DIR="$1"
+INSTALL_DIR="$2"
+
+if [ ! -d "$OPENSSL_DIR" ]; then
+  git clone --depth=1 --branch OpenSSL_1_1_1f https://github.com/openssl/openssl.git $OPENSSL_DIR
+fi
+
+cd $OPENSSL_DIR
+
+# patch1
+../patches/apply_patch.sh \
+  crypto/rand/rand_lib.c \
+  ../patches/openssl1.patch
+
+source /opt/emsdk/emsdk_env.sh
+
+emconfigure ./config no-shared no-asm no-threads no-dso
+sed -i 's|^CROSS_COMPILE.*$|CROSS_COMPILE=|g' Makefile
+emmake make build_generated libcrypto.a libssl.a
+#emmake make install_sw
+
+# patch2 
+../patches/apply_patch.sh \
+  include/openssl/opensslconf.h \
+  ../patches/openssl2.patch
