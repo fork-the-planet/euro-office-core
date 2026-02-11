6fc96a3bad2b20aed178296e84433de414244ebd
Hendrik Leidinger <hendrik.leidinger@nextcloud.com>
2026-02-11 15:01:31 -0800

    finished hash for now, dont know how to test
    Signed-off-by: Hendrik Leidinger <hendrik.leidinger@nextcloud.com>

--- DesktopEditor/xmlsec/src/wasm/hash/hash_base_post.js
+++ DesktopEditor/xmlsec/src/wasm/hash/hash_base_post.js
@@ -1,94 +1,4 @@
-/*
- * (c) Copyright Ascensio System SIA 2010-2019
- *
- * This program is a free software product. You can redistribute it and/or
- * modify it under the terms of the GNU Affero General Public License (AGPL)
- * version 3 as published by the Free Software Foundation. In accordance with
- * Section 7(a) of the GNU AGPL its Section 15 shall be amended to the effect
- * that Ascensio System SIA expressly excludes the warranty of non-infringement
- * of any third-party rights.
- *
- * This program is distributed WITHOUT ANY WARRANTY; without even the implied
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR  PURPOSE. For
- * details, see the GNU AGPL at: http://www.gnu.org/licenses/agpl-3.0.html
- *
- * You can contact Ascensio System SIA at 20A-12 Ernesta Birznieka-Upisha
- * street, Riga, Latvia, EU, LV-1050.
- *
- * The  interactive user interfaces in modified source and object code versions
- * of the Program must display Appropriate Legal Notices, as required under
- * Section 5 of the GNU AGPL version 3.
- *
- * Pursuant to Section 7(b) of the License you must retain the original Product
- * logo when distributing the program. Pursuant to Section 7(e) we decline to
- * grant you any rights under trademark law for use of our trademarks.
- *
- * All the Product's GUI elements, including illustrations and icon sets, as
- * well as technical writing content are licensed under the terms of the
- * Creative Commons Attribution-ShareAlike 4.0 International. See the License
- * terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
- *
- */
-
-//base64
-
-(function(window, undefined) {
-
-	window.messageData = null;
-	window.messagePort = null;
-	function onMessageEvent(data, port)
-	{
-	    if (data.type == "hash")
-	    {
-	        window.messageData = data.value;
-	        window.messagePort = port;
-	        if (!window.engineInit)
-	        	return;
-	        checkMessage();
-	    }
-	}
-
-	window.onconnect = function(e)
-	{
-	    var port = e.ports[0];
-	    port.onmessage = function(e) {
-	        onMessageEvent(e.data, port);
-	    }    
-	};
-	window.onmessage = function(e)
-	{
-	    onMessageEvent(e.data);
-	};
-	window.engineInit = false;
-	window.onEngineInit = function()
-	{
-		window.engineInit = true;
-		if (window.messageData)
-			checkMessage();
-	};
-
-	function checkMessage()
-	{
-		var data = window.messageData;
-		var res = [];
-
-		for (var i = 0, len = data.length; i < len; i++)
-        {
-            res.push(AscCommon.Hash.hashOffice(data[i].password, data[i].salt, data[i].spinCount, data[i].alg).base64());
-        }
-
-		var sender = window.messagePort || window;
-		sender.postMessage({ hashValue : res });
-	}
-
-	//desktop_fetch
-	
-	//polyfill
-	
-	//string_utf8
     
-    //module
-
     var HashAlgs = {
         MD2       : 0,
         MD4       : 1,
@@ -218,5 +128,9 @@
 
 		return result;
 	};
+
+    Module.onRuntimeInitialized = function () {
+        self.onEngineInit();
+    };
 
 })(self, undefined);
